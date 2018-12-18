c0a424448aafb3436478ccb5d4b2c3d0c48c0677
abellynow <abellynow@users.noreply.github.com>
2018-12-18 01:17:42 +0100

    Add basic webserving
    By setting up a simple webserver there's no longer a need to setup and
    run the Spotify authorization sample. WeeklySaver can now handle initial
    authorization as well as refreshing tokens.

--- weeklysaver.py
+++ weeklysaver.py
@@ -8,7 +8,8 @@ import datetime
 import hashlib
 import json
 import argparse
-#from wsgiref import simple_server, util
+import urllib
+from wsgiref import simple_server, util
 
 class Bearer(requests.auth.AuthBase):
   def __init__(self, access_token):
@@ -19,6 +20,14 @@ class Bearer(requests.auth.AuthBase):
     else: # assume bytes/bytearray
       r.headers['Authorization'] = b'Bearer ' + self.access_token
     return r
+
+def rand_ascii(count):
+  chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  rnd = os.urandom(count)
+  rv = ''
+  for i in range(count):
+    rv += chars[rnd[i] % len(chars)]
+  return rv
 
 def read_config(filename):
   rv = dict()
@@ -38,6 +47,14 @@ def write_config(filename, vals):
     for k, v in vals.items():
       print("{}={}".format(k, v if v != None else ""), file = cfg)
 
+def query_split(q):
+  rv = dict()
+  l = q.split('&')
+  for a in l:
+    k, v = a.split('=')
+    rv[k] = urllib.parse.unquote(v)
+  return rv
+
 def hash_file(filename):
   with open(filename, 'rb') as f:
     return hashlib.sha1(f.read()).hexdigest()
@@ -46,6 +63,8 @@ def hash_str(t):
   return hashlib.sha1(t).hexdigest()
 
 class Spotify(object):
+  state_key = 'spotify_auth_state'
+
   def __init__(self, basedir, debug = False):
     self.basedir = basedir
     if debug:
@@ -87,6 +106,24 @@ class Spotify(object):
         trying = 0
     rv = r.text
     return rv
+  def create_auth_uri(self, redirect_uri):
+    state = rand_ascii(16)
+    scope = 'playlist-read-private'
+    query = "https://accounts.spotify.com/authorize?response_type=code&client_id={}&scope={}&redirect_uri={}&state={}".format(self.keys['client_id'], scope, redirect_uri, state)
+    cookie = "{}={}".format(self.state_key, state)
+    return query, cookie
+  def authorize(self, code, redirect_uri):
+    r = requests.post('https://accounts.spotify.com/api/token',
+                    auth = (self.keys['client_id'], self.keys['client_secret']),
+                    data = {'code': code, 'redirect_uri': redirect_uri, 'grant_type': 'authorization_code'})
+    if r.status_code != 200:
+      return False
+    jsb = r.json()
+    self.session['access_token'] = jsb['access_token']
+    if 'refresh_token' in jsb:
+      self.session['refresh_token'] = jsb['refresh_token']
+    self.save_tokens()
+    return True
   def _find_list_in_json(self, jd, name):
     for pl in jd['items']:
       if pl['name'] == name:
@@ -176,15 +213,81 @@ class WeeklySaver(object):
     for it in pjl['items']:
       track = it['track']
       print("{}/{}/{}".format(track['name'], ','.join([tn['name'] for tn in track['artists']]), track['album']['name']))
+  def serve(self, port):
+    self.port = port
+    self.handlers = {'/': self._index, '/login': self._login, '/callback': self._callback, '/favicon.ico': self._not_found, '/success': self._success, '/failure': self._failure}
+    self.httpd = simple_server.make_server('', port, self._serve)
+    self.httpd.serve_forever()
+  def _not_found(self, parts, environ, respond):
+    respond('404 Not Found', [('Content-Type', 'text/plain')])
+    return [b'not found']
+  def _success(self, parts, environ, respond):
+    doc = "<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"UTF-8\">\n<title>WeeklySaver</title>\n<body>\n"
+    doc += "<h2>Success! WeeklySaver can now save your Discover Weekly</h2>\n"
+    doc += "<div><a href=\"/login\">Login to Spotify</a></div>"
+    doc += "</body></html>"
+    respond('200 OK', [('Content-Type', 'text/html')])
+    return [doc.encode('utf-8')]
+  def _failure(self, parts, environ, respond):
+    doc = "<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"UTF-8\">\n<title>WeeklySaver</title>\n<body>\n"
+    doc += "<h2>Failure! WeeklySaver cannot access your Discover Weekly</h2>\n"
+    doc += "<div><a href=\"/login\">Login to Spotify</a></div>"
+    doc += "</body></html>"
+    respond('200 OK', [('Content-Type', 'text/html')])
+    return [doc.encode('utf-8')]
+  def _index(self, parts, environ, respond):
+    doc = "<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"UTF-8\">\n<title>WeeklySaver</title>\n<body>"
+    doc += "<div><a href=\"/login\">Login to Spotify</a></div>"
+    doc += "</body></html>"
+    respond('200 OK', [('Content-Type', 'text/html')])
+    return [doc.encode('utf-8')]
+  def _login(self, parts, environ, respond):
+    redirect_uri = "http://localhost:{}/callback".format(self.port)
+    auth_uri, cookie = self.sptfy.create_auth_uri(redirect_uri)
+    respond('301 Moved Permanently', [('Location', auth_uri), ('Set-Cookie', "{}".format(cookie))])
+    return [b'301 Moved Permanently']
+  def _callback(self, parts, environ, respond):
+    redirect_uri = "http://localhost:{}/callback".format(self.port)
+    query = query_split(environ['QUERY_STRING'])
+    cookies = query_split(environ['HTTP_COOKIE'])
+    code = query.get('code')
+    state = query.get('state')
+    storedState = cookies.get(self.sptfy.state_key)
+    end_uri = "/failure"
+    if state == None or state != storedState:
+      print("ERROR! Bad state")
+    else:
+      if self.sptfy.authorize(code, redirect_uri):
+        print("Got me some tokens!")
+        end_uri = "/success"
+    respond('301 Moved Permanently', [('Location', end_uri)])
+    return [b'301 Moved Permanently']
+  def _serve(self, environ, respond):
+    try:
+      parts = [p for p in environ['PATH_INFO'].split('/') if len(p) > 0]
+      f = self.handlers['/' + parts[0] if len(parts) > 0 else '/']
+      return f(parts, environ, respond)
+    except KeyError:
+      respond('404 Not Found', [('Content-Type', 'text/plain')])
+      return [b'not found']
 
-if __name__ == "__main__":
+
+def main_prog():
   parser = argparse.ArgumentParser(description = 'WeeklySaver - saves your Spotify Discover Weekly')
+  parser.add_argument('--serve', action = 'store_true', default = False, help = 'serve web page')
+  parser.add_argument('-p', '--port', type = int, dest = 'port', default = 8888, help = 'website port number')
   parser.add_argument('--show', action = 'store_true', default = False, help = 'show tracks for a week')
   parser.add_argument('-w', '--week', type = int, dest = 'week', default = None, help = 'week to show')
   args = parser.parse_args()
+
   basedir = os.path.join(os.path.expanduser("~"), ".weeklysaver")
   ws = WeeklySaver(basedir = basedir)
   if args.show:
     ws.list_songs_for_week(args.week if args.week != None else get_this_week())
+  elif args.serve:
+    ws.serve(args.port)
   else:
     ws.retrieve_weekly()
+
+if __name__ == "__main__":
+  main_prog()
